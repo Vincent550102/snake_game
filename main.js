307f3396b089a395e337c932a5a62f4fd264a59c
Vincent <5020559@gmail.com>
2021-02-04 21:15:14 +0800

    graph upd

--- main.js
+++ main.js
@@ -10,8 +10,12 @@ $(document).ready(function () {
     getData(url)
     .then(data => {
         $("#first").children().remove();
+        $("#second").children().remove();
+        $("#third").children().remove();
         const alluserDatas = data.datas
         $("#first").text("1st."+alluserDatas[0].uid+"(score:"+alluserDatas[0].score+")")
+        $("#second").text("2nd."+alluserDatas[1].uid+"(score:"+alluserDatas[1].score+")")
+        $("#third").text("3rd."+alluserDatas[2].uid+"(score:"+alluserDatas[2].score+")")
     })
     .catch(error => console.error(error))
 });
